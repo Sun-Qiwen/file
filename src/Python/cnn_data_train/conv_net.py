ae1fa8ecde5a183d6827dc3027f1eeae0925c110
Sun-Qiwen <2957594649@qq.com>
2026-09-26 13:18:31 +0800

    Update conv_net.py

--- src/Python/cnn_data_train/conv_net.py
+++ src/Python/cnn_data_train/conv_net.py
@@ -7,7 +7,7 @@ import pickle
 
 class ConvNet:
     # 初始化
-    def __init__(self, input_dim=(1, 28, 28),
+    def __init__(self, input_dim=(1, 96, 96),
                  conv_param={'filter_num': 30, 'filter_size': 5, 'filter_pad': 0, 'filter_stride': 1},
                  hidden_size=100, output_size=10, weight_init_std=0.01):
 
